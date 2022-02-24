bd15798ff31f37a1c48ab29cef9ae1f5b2a2e98f
BurwitzBlagger <99707350+BurwitzBlagger@users.noreply.github.com>
2022-02-24 17:41:56 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+             printf("We're no strangers to love\n");
+
+             printf("You know the rules, and so do I\n");
+
+             printf("A full commitment's what I'm thinking of\n");
+
+             printf("You won't get this from any other guy\n");
+
+             printf("I.....");
+
+             printf("Just wanna tell you how I'm feeling\n");
+
+             printf("Gotta make you");
+
+             printf(" understand\n");
+
+             printf("Never gonna give you up\n");
+
+             printf("Never gonna let you down\n");
+
+             printf("Never gonna run around and desert you\n");
+
+             printf("Never gonna make you cry\n");
+
+             printf("Never gonna say goodbye\n");
+
+             printf("Never gonna tell a lie and hurt you\n");
+
+             printf("We've known each other");
+
+             printf(" for so long\n");
+
+             printf("Your heart's been aching but");
+
+             printf(" you're too shy to say it\n");
+
+             printf("Inside we both know what's been");
+
+             printf(" going on\n");
+
+             printf("We know the game and we're");
+
+             printf(" gonna play it\n");
+
+             printf("And.......");
+
+             printf("if you ask me how I'm feeling\n");
+
+             printf("Don't tell me you're too");
+
+             printf(" blind to see\n");
+
+             printf("Never gonna give you up\n");
+
+             printf("Never gonna let you down\n");
+
+             printf("Never gonna run around and desert you\n");
+
+             printf("Never gonna make you cry\n");
+
+             printf("Never gonna say goodbye\n");
+
+             printf("Never gonna tell a lie and hurt you\n");
+
+             printf("Never gonna give you up\n");
+
+             printf("Never gonna let you down\n");
+
+             printf("Never gonna run around and desert you\n");
+
+             printf("Never gonna make you cry\n");
+
+             printf("Never gonna say goodbye\n");
+
+             printf("Never gonna tell a lie and hurt you\n");
+
+             printf("Never gonna give you up\n");
+
+             printf("Never gonna let you down\n");
+
+             printf("Never gonna run around and desert you\n");
+
+             printf("Never gonna make you cry\n");
+
+             printf("Never gonna say goodbye\n");
+
+             printf("Never gonna tell a lie and hurt you\n");
+
+             system("PAUSE");
+}
